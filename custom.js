9bccf15867e2dd13d91da16d5e78f5ed36328a19
praba219 <prabhashiperera325@gmail.com>
2025-02-12 15:54:41 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,14 @@
+ $(function () {
+  // Main Menu JS
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 50) {
+      $("nav").removeClass("site-top-nav");
+      $("#back-to-top").fadeOut();
+    } else {
+      $("nav").addClass("site-top-nav");
+      $("#back-to-top").fadeIn();
+    }
+  });
+
+
+  
